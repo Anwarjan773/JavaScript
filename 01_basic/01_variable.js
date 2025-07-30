e27e5c408fd63e37d2b2e9903e60a3fa893406a0
Anwar jan <anwarjan773733@gmail.com>
2025-07-30 04:47:47 +0000

    define declerations of variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const age = 19;
+console.log(age);
+
+
+let firstName = "Anwar ";
+// let firstName = "Anwar khan";
+firstName ="Ail";
+console.log(firstName);
+
+var fullName = "Kashif Ali";
+var fullName = "kashif Bunneri";
+console.log(fullName);
+
